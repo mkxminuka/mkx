87b888398f63ec5633b31222d833cf2b414b8ae3
mkxminuka <91891451+mkxminuka@users.noreply.github.com>
2021-10-04 18:31:20 +0530

    Add files via upload

---
+++ PhoenixMiner-5.5c/start.bat
@@ -0,0 +1,3 @@
+@echo off
+PhoenixMiner.exe -pool ssl://us1-etc.ethermine.org:5555 -pool2 ssl://eu1-etc.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.qg2u5s89u0h88p3 -gpow 96
+pause
